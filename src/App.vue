8257a7ce1dbfcd00266a433e63470082b44ab459
Quoc Mien <quocmienn@gmail.com>
2023-11-05 11:34:51 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,133 @@
 <script setup>
-</script>
+import product from './components/product-car.vue'
+import checkbox from './components/checkbox-product.vue'
 
+  const productsCar = {
+    products: [
+    {
+      url: '../assets/img/car.jpg',
+      name: 'BUICK Cascada-Black 2017',
+      promotion: '2017 BUICK Cascada-Black GAID: Cascada0765',
+      desc: '715660E',
+      vin: 'W04WH3N54HG110765',
+      regis_exp: '05/31/2024 ',
+      ga_id: 'Cascada0765',
+      features: 'Automatic transmission, AUX input, Convertible, USB input, Bluetooth, Backup Camera, Cruise Control',
+      location: ' 450 S Siesta Ln, Tempe, AZ 85288 ',
+      status: 'ARIZONA -ON TRIP-NOT AVAILABLE',
+      btn: [
+        {
+          title: 'GA KEY'
+        },
+                {
+          title: 'GA KEY'
+        },
+                {
+          title: 'GA KEY'
+        },
+                {
+          title: 'GA KEY'
+        },
+                {
+          title: 'GA KEY'
+        },
+      ]
+    },
+    {
+      name: 'BUICK Cascada-Black 2017',
+      promotion: '2017 BUICK Cascada-Black GAID: Cascada0765',
+      desc: '715660E',
+      vin: 'W04WH3N54HG110765',
+      regis_exp: '05/31/2024 ',
+      ga_id: 'Cascada0765',
+      features: 'Automatic transmission, AUX input, Convertible, USB input, Bluetooth, Backup Camera, Cruise Control',
+      location: ' 450 S Siesta Ln, Tempe, AZ 85288 ',
+      status: 'ARIZONA -ON TRIP-NOT AVAILABLE'
+    },
+    {
+      name: 'BUICK Cascada-Black 2017',
+      promotion: '2017 BUICK Cascada-Black GAID: Cascada0765',
+      desc: '715660E',
+      vin: 'W04WH3N54HG110765',
+      regis_exp: '05/31/2024 ',
+      ga_id: 'Cascada0765',
+      features: 'Automatic transmission, AUX input, Convertible, USB input, Bluetooth, Backup Camera, Cruise Control',
+      location: ' 450 S Siesta Ln, Tempe, AZ 85288 ',
+      status: 'ARIZONA -ON TRIP-NOT AVAILABLE'
+    },
+    {
+      name: 'BUICK Cascada-Black 2017',
+      promotion: '2017 BUICK Cascada-Black GAID: Cascada0765',
+      desc: '715660E',
+      vin: 'W04WH3N54HG110765',
+      regis_exp: '05/31/2024 ',
+      ga_id: 'Cascada0765',
+      features: 'Automatic transmission, AUX input, Convertible, USB input, Bluetooth, Backup Camera, Cruise Control',
+      location: ' 450 S Siesta Ln, Tempe, AZ 85288 ',
+      status: 'ARIZONA -ON TRIP-NOT AVAILABLE'
+    }
+  ]
+  }
+
+  const checkboxList = {
+    features : [
+      {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },
+      {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },    {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },    {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },
+    ],
+    make : [
+      {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },
+      {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },    {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },    {
+        option: 'all-wheel-drive',
+        label: 'All-wheel dive'
+      },
+    ]
+  }
+
+</script>
 <template>
-  <div class="page-minarental container-fluid">
-    <header class="header">
-      <el-input></el-input>
-    </header>
+  <div class="common-layout">
+    <el-container>
+      <el-aside 
+        width="200px"
+        
+      >
+         <checkbox
+            v-bind="checkboxList"
+         />
+      </el-aside>
+      <el-main>
+        <product 
+          v-bind="productsCar"
+        />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
-<style scoped>
+
+
+<style >
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -22,4 +140,6 @@
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
+
 </style>
---
+++ src/components/checkbox-product.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="checkbox ">
+    <h3>Features</h3>
+    <div 
+      class="checkbox-features"
+      v-for="(item, index) in features"
+      :key="index"
+    >
+      <el-checkbox :v-model="item.option" :label="item.label" size="large" />
+    </div>
+    <h3>Make</h3>
+    <div 
+      class="checkbox-make"
+      v-for="(item, index) in make"
+      :key="index"
+    >
+      <el-checkbox :v-model="item.option" :label="item.label" size="large" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  features: {
+    type: Array,
+    default: [],
+  },
+  make: {
+    type: Array,
+    default: [],
+  }
+
+});
+</script>
+
+<style lang="scss">
+
+</style>
